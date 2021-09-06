704a16c722a8190becf7090c5af76f5a5f62f687
TeonaTanasa <teona.tanasa@principal33.com>
2021-09-06 18:18:29 +0300

    added photo column.

--- js/index.js
+++ js/index.js
@@ -51,6 +51,8 @@ function addTableRow() {
     cell4 = newRow.insertCell(3);
     cell5 = newRow.insertCell(4);
     cell6 = newRow.insertCell(5);
+    cell7 = newRow.insertCell(6);
+    photo = document.getElementById("photo").value;
     fname = document.getElementById("fname").value;
     lname = document.getElementById("lname").value;
     email = document.getElementById("email").value;
@@ -60,13 +62,14 @@ function addTableRow() {
     actions = `<button onclick="editTableRow();" class="edit-icon"> <i class="fas fa-edit fa-2x"></i></button>
               <button onclick="deleteSelectedRow();" class="delete-icon"><i class="fas fa-times fa-2x"></i> </button>`;
 
-    cell1.innerHTML = fname;
-    cell2.innerHTML = lname;
-    cell3.innerHTML = email;
-    cell4.innerHTML = gender;
-    cell5.innerHTML = formatedBirthdate.format("DD MMM YYYY");
-    cell6.innerHTML = actions;
-    selectedRowToInput();
+    cell1.innerHTML = photo;
+    cell2.innerHTML = fname;
+    cell3.innerHTML = lname;
+    cell4.innerHTML = email;
+    cell5.innerHTML = gender;
+    cell6.innerHTML = formatedBirthdate.format("DD MMM YYYY");
+    cell7.innerHTML = actions;
+    clearField();
   }
 }
 
@@ -75,20 +78,20 @@ function selectedRowToInput() {
     table.rows[i].onclick = function () {
       // get the selected row index
       rIndex = this.rowIndex;
-      document.getElementById("fname").value = this.cells[0].innerHTML;
-      document.getElementById("lname").value = this.cells[1].innerHTML;
-      document.getElementById("email").value = this.cells[2].innerHTML;
-      document.getElementById("gender").value = this.cells[3].innerHTML;
-      var formatedBirthdate = moment(this.cells[4].innerHTML);
+      document.getElementById("photo").value = this.cells[0].innerHTML;
+      document.getElementById("fname").value = this.cells[1].innerHTML;
+      document.getElementById("lname").value = this.cells[2].innerHTML;
+      document.getElementById("email").value = this.cells[3].innerHTML;
+      document.getElementById("gender").value = this.cells[4].innerHTML;
+      var formatedBirthdate = moment(this.cells[5].innerHTML);
       document.getElementById("birthdate").value = formatedBirthdate.format("YYYY-MM-DD");
     };
   }
 }
 
-selectedRowToInput();
-
 function editTableRow() {
   selectedRowToInput();
+  photo = document.getElementById("photo").value;
   fname = document.getElementById("fname").value;
   lname = document.getElementById("lname").value;
   email = document.getElementById("email").value;
@@ -96,11 +99,12 @@ function editTableRow() {
   birthdate = document.getElementById("birthdate").value;
   if (!checkEmptyInput()) {
     var currentRow = table.rows[rIndex];
-    currentRow.cells[0].innerHTML = fname;
-    currentRow.cells[1].innerHTML = lname;
-    currentRow.cells[2].innerHTML = email;
-    currentRow.cells[3].innerHTML = gender;
-    currentRow.cells[4].innerHTML = birthdate;
+    currentRow.cells[0].innerHTML = photo;
+    currentRow.cells[1].innerHTML = fname;
+    currentRow.cells[2].innerHTML = lname;
+    currentRow.cells[3].innerHTML = email;
+    currentRow.cells[4].innerHTML = gender;
+    currentRow.cells[5].innerHTML = birthdate;
   }
 }
 
@@ -108,11 +112,14 @@ function deleteSelectedRow() {
   selectedRowToInput();
   if (typeof rIndex == "number") {
     table.deleteRow(rIndex);
-    // clear input text
-    document.getElementById("fname").value = "";
-    document.getElementById("lname").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("gender").value = "";
-    document.getElementById("birthdate").value = "";
+    clearField();
   }
+}
+
+function clearField(){
+  document.getElementById("fname").value = "";
+  document.getElementById("lname").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("gender").value = "";
+  document.getElementById("birthdate").value = "";
 }
